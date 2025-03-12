2c2efb7f4758601e0c3e15ad217e078fe8d9ca37
IShix <49128115+IShix-g@users.noreply.github.com>
2025-03-12 09:59:46 +0900

    refactor: update variable naming and add empty loop

--- Packages/SmartGitUPM/Editor/LintTest.cs
+++ Packages/SmartGitUPM/Editor/LintTest.cs
@@ -2,13 +2,16 @@ namespace SmartGitUPM.Editor
 {
     public sealed class LintTest
     {
-      static string s_staticField = string.Empty;
+        static string s_staticField = string.Empty;
         string _test;
-        string test2;
+        string _test2;
 
         void Test()
         {
+            for(var i = 0; i < 5; i++)
+            {
 
+            }
         }
     }
 }
