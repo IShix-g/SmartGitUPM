aed42638ceb3c61263e4ed9503822137e1b6beb7
IShix <49128115+IShix-g@users.noreply.github.com>
2025-03-12 09:57:07 +0900

    feat: add initial LintTest class to SmartGitUPM package

---
+++ Packages/SmartGitUPM/Editor/LintTest.cs
@@ -0,0 +1,14 @@
+namespace SmartGitUPM.Editor
+{
+    public sealed class LintTest
+    {
+      static string s_staticField = string.Empty;
+        string _test;
+        string test2;
+
+        void Test()
+        {
+
+        }
+    }
+}
